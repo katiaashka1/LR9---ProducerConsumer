6b48d34a52b4b84314e429b7a14d67d0fed9531c
kelril <kelril@dsu1.service.sarnet>
2020-05-01 17:03:26 +0300

    add processing input data

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,16 +1,33 @@
 // Copyright 2018 Your Name <your_email>
 
-#include <header.hpp>
+#include "header.hpp"
+
 
 int main(int argc, char **argv) {
-    if (argc) {
-        std::string url = argv[0];
-        uint64_t depth = std::stoi(argv[1]);
-        uint64_t network_threads = std::stoi(argv[2]);
-        uint64_t parser_threads = std::stoi(argv[3]);
-        std::string output = argv[4];
-        Crawler s(url, depth, network_threads, parser_threads, output);
-        s.handler();
+
+    boost::program_options::options_description desc("asd");
+    desc.add_options()
+            ("url", boost::program_options::value<std::string>())
+            ("depth", boost::program_options::value<unsigned>())
+            ("network_threads", boost::program_options::value<unsigned>())
+            ("parser_threads", boost::program_options::value<unsigned>())
+            ("output", boost::program_options::value<std::string>());
+    boost::program_options::variables_map vm;
+    try {
+        boost::program_options::store(boost::program_options::
+                                      parse_command_line(argc, argv, desc), vm);
+        boost::program_options::notify(vm);
     }
+    catch (boost::program_options::error &e) {
+        std::cout << e.what() << std::endl;
+    }
+    
+    Crawler s(vm["url"].as<std::string>(),
+              vm["depth"].as<unsigned>(),
+              vm["network_threads"].as<unsigned>(),
+              vm["parser_threads"].as<unsigned>(),
+              vm["output"].as<std::string>());
+    s.handler();
+
     return 0;
 }
